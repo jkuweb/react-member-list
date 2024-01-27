2ccfdff64f6c1c8290868b90ab83d9cd71f4d39f
Joseba <josebaquerejetaweb@gmail.com>
2024-01-27 11:52:03 +0100

    implement form  JSX en LoginPage

--- src/login.page.tsx
+++ src/login.page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export const LoginPage: React.FC = () => {
+  const [userName, setUserName] = React.useState('')
+  const [password, setPassword] = React.useState('')
   return (
     <>
-      <h1>Hello from Login Page</h1>
-      <Link to="/list">Go to List</Link>
-    </>
+      <h1>Login</h1>
+      <label htmlFor="userName">Name:</label>
+      <input type="text" id="userName" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="Please, insert your name" />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Please, insert your password"/>
+      <button>Enter</button>
+      
+       </>
   )
 }
