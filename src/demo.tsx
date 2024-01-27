600e0dae2a27f56cf5091f448d9b7d39f610a4e8
Joseba <josebaquerejetaweb@gmail.com>
2024-01-27 10:48:45 +0100

    implement useNavigate Hook in DetailPage

--- src/demo.tsx
+++ src/demo.tsx
@@ -5,21 +5,23 @@ import { DetailPage } from "./detail.page";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const AppRouter = () => {
-  return(
-  <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<LoginPage/>}/>
-      <Route path='/list' element={<ListPage/>}/>
-      <Route path='/detail' element={<DetailPage/>}/>
-     </Routes>
-  </BrowserRouter>
-
-  ) 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/detail" element={<DetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export const Demo: React.FC = () => {
   return (
     <>
+      <header>
+        <h1>App Title</h1>
+      </header>
       <AppRouter />
     </>
   );
--- src/detail.page.tsx
+++ src/detail.page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const DetailPage: React.FC = () => {
+  const navigate = useNavigate()
+
   return (
     <>
       <h3>Hello from Detail Page</h3>
-      <Link to="/">Go Home</Link>
+      <button onClick={() => navigate(-1)}>Atras</button>
     </>
   )
 }
