f2ed6826cfa149d31877466f29407ce77d465011
Joseba <josebaquerejetaweb@gmail.com>
2024-01-27 12:15:06 +0100

    01-refactoring -> setFormData : extract JSX inline functions to two arrow functions

--- src/login.page.tsx
+++ src/login.page.tsx
@@ -2,23 +2,43 @@ import React from "react";
 
 export const LoginPage: React.FC = () => {
   const [userFormData, setUserFormData] = React.useState({
-    userName: '',
-    password: ''
-  })
+    userName: "",
+    password: "",
+  });
+
+  const handleChangeUserName = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setUserFormData({
+      ...userFormData,
+      userName: e.target.value,
+    });
+  };
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserFormData({
+      ...userFormData,
+      password: e.target.value,
+    });
+  };
+
   return (
     <>
       <h1>Login</h1>
       <label htmlFor="userName">Name:</label>
-      <input type="text" id="userName" value={userFormData.userName} onChange={(e) => setUserFormData({
-        ...userFormData,
-        userName: e.target.value
-      })} placeholder="Please, insert your name" />
+      <input
+        type="text"
+        id="userName"
+        value={userFormData.userName}
+        onChange={handleChangeUserName}
+        placeholder="Please, insert your name"
+      />
       <label htmlFor="password">Password:</label>
-      <input type="password" id="password" value={userFormData.password} onChange={(e) => setUserFormData({
-        ...userFormData,
-        password: e.target.value})} placeholder="Please, insert your password"/>
+      <input
+        type="password"
+        id="password"
+        value={userFormData.password}
+        onChange={handleChangePassword}
+        placeholder="Please, insert your password"
+      />
       <button>Enter</button>
-      
-       </>
-  )
-}
+    </>
+  );
+};
