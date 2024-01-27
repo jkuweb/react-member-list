23f3fee781a261b8e76d2019b2adb8f2e4198b92
Joseba <josebaquerejetaweb@gmail.com>
2024-01-27 10:08:33 +0100

    Install react-router-dom and implement AppRouter in Demo Componente

--- src/demo.tsx
+++ src/demo.tsx
@@ -2,26 +2,25 @@ import React from "react";
 import { LoginPage } from "./login.page";
 import { ListPage } from "./list.page";
 import { DetailPage } from "./detail.page";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const AppRouter = () => {
+  return(
+  <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<LoginPage/>}/>
+      <Route path='/list' element={<ListPage/>}/>
+      <Route path='/detail' element={<DetailPage/>}/>
+     </Routes>
+  </BrowserRouter>
+
+  ) 
+};
 
 export const Demo: React.FC = () => {
-  const path = window.location.pathname;
-
-  const renderComponent = (path: string) => {
-    switch (path) {
-      case '/':
-        return <LoginPage/>
-      case '/list':
-        return <ListPage/>
-      case '/detail':
-        return <DetailPage/>
-      default:
-        return <div>404 Not Found</div>  
-    }
-  }
-
   return (
     <>
-      {renderComponent(path)}
+      <AppRouter />
     </>
   );
 };
