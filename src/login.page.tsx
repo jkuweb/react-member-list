266cf521ae3998462794bc2c9b802da734e78f2b
Joseba <josebaquerejetaweb@gmail.com>
2024-01-27 12:03:32 +0100

    change userName and password string states to a userFormData object state

--- src/login.page.tsx
+++ src/login.page.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 
 export const LoginPage: React.FC = () => {
-  const [userName, setUserName] = React.useState('')
-  const [password, setPassword] = React.useState('')
+  const [userFormData, setUserFormData] = React.useState({
+    userName: '',
+    password: ''
+  })
   return (
     <>
       <h1>Login</h1>
       <label htmlFor="userName">Name:</label>
-      <input type="text" id="userName" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="Please, insert your name" />
+      <input type="text" id="userName" value={userFormData.userName} onChange={(e) => setUserFormData({
+        ...userFormData,
+        userName: e.target.value
+      })} placeholder="Please, insert your name" />
       <label htmlFor="password">Password:</label>
-      <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Please, insert your password"/>
+      <input type="password" id="password" value={userFormData.password} onChange={(e) => setUserFormData({
+        ...userFormData,
+        password: e.target.value})} placeholder="Please, insert your password"/>
       <button>Enter</button>
       
        </>
