d3abc46d313a1f7cff95aae926f8f1ec75e239f7
Joseba <josebaquerejetaweb@gmail.com>
2024-01-28 18:28:39 +0100

    change string Route paths to switchRoutes

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { LoginPage, ListPage, DetailPage } from "@/scenes";
+import { switchRoutes } from "./routers";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/detail/:login" element={<DetailPage />} />
+        <Route path={switchRoutes.root} element={<LoginPage />} />
+        <Route path={switchRoutes.list} element={<ListPage />} />
+        <Route path={switchRoutes.detail} element={<DetailPage />} />
         <Route path="/*" element={<div>404 Not Found</div>} />
       </Routes>
     </BrowserRouter>
