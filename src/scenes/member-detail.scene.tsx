474c7172aba3e64eddbaf82232126b5e49a814a9
Joseba <josebaquerejetaweb@gmail.com>
2024-01-30 20:44:32 +0100

    refactor detail link in MemberList component, now scene decide it

--- src/scenes/member-detail.scene.tsx
+++ src/scenes/member-detail.scene.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { AppLayout } from "@/layouts";
 import { MemberDetailContainer } from "@/pods/member-detail";
+import { useParams } from "react-router-dom";
 
 export const MemberDetailScene: React.FC = () => {
-  
+  const {login} = useParams<{login: string}>()
   return (
     <AppLayout>
-      <MemberDetailContainer/>
+      <MemberDetailContainer login={login}/>
     </AppLayout>
   );
 };
